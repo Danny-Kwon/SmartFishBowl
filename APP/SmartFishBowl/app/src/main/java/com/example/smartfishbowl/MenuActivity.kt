85e099d03f369ed192f9077d7237076e37c0cef0
Minhyeok <danny9kwon@gmail.com>
2022-11-25 10:45:06 +0900

    값변경 추가

--- APP/SmartFishBowl/app/src/main/java/com/example/smartfishbowl/MenuActivity.kt
+++ APP/SmartFishBowl/app/src/main/java/com/example/smartfishbowl/MenuActivity.kt
@@ -1,18 +1,25 @@
 package com.example.smartfishbowl
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.smartfishbowl.databinding.AlertdialogEdittextBinding
+import com.example.smartfishbowl.viewmodel.BowlViewModel
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_drawer.*
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     lateinit var navigationView: NavigationView
     lateinit var drawerLayout: DrawerLayout
+    lateinit var viewModel: BowlViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drawer)
@@ -24,6 +31,102 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawerLayout = findViewById(R.id.activity_drawer)
         navigationView = findViewById(R.id.main_navigationView)
         navigationView.setNavigationItemSelectedListener(this)
+        //bowl.text = api. XXX or prefs.setString
+        change_tmp.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val builderItem = AlertdialogEdittextBinding.inflate(layoutInflater)
+            val edittext = builderItem.editText
+            with(builder){
+                setTitle("적정 온도 변경")
+                setMessage("적정 온도를 입력하세요.")
+                setView(builderItem.root)
+                setPositiveButton("확인"){ _: DialogInterface, _: Int ->
+                    if(edittext.text!=null) {
+                        tmp.text = edittext.text
+                    }
+                }
+                setNegativeButton("취소"){ _: DialogInterface, _: Int ->
+                    Toast.makeText(context, "적정 온도 변경 취소", Toast.LENGTH_SHORT).show()
+                }
+                show()
+            }
+        }
+        change_hgt.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val builderItem = AlertdialogEdittextBinding.inflate(layoutInflater)
+            val edittext = builderItem.editText
+            with(builder){
+                setTitle("적정 수위 변경")
+                setMessage("적정 수위를 입력하세요.")
+                setView(builderItem.root)
+                setPositiveButton("확인"){ _: DialogInterface, _: Int ->
+                    if(edittext.text!=null) {
+                        hgt.text = edittext.text
+                    }
+                }
+                setNegativeButton("취소"){ _: DialogInterface, _: Int ->
+                    Toast.makeText(context, "적정 수위 변경 취소", Toast.LENGTH_SHORT).show()
+                }
+                show()
+            }
+        }
+        change_ph.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val builderItem = AlertdialogEdittextBinding.inflate(layoutInflater)
+            val edittext = builderItem.editText
+            with(builder){
+                setTitle("적정 PH 변경")
+                setMessage("적정 PH를 입력하세요.")
+                setView(builderItem.root)
+                setPositiveButton("확인"){ _: DialogInterface, _: Int ->
+                    if(edittext.text!=null) {
+                        ph.text = edittext.text
+                    }
+                }
+                setNegativeButton("취소"){ _: DialogInterface, _: Int ->
+                    Toast.makeText(context, "적정 PH 변경 취소", Toast.LENGTH_SHORT).show()
+                }
+                show()
+            }
+        }
+        change_drt.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val builderItem = AlertdialogEdittextBinding.inflate(layoutInflater)
+            val edittext = builderItem.editText
+            with(builder){
+                setTitle("적정 탁도 변경")
+                setMessage("적정 탁도를 입력하세요.")
+                setView(builderItem.root)
+                setPositiveButton("확인"){ _: DialogInterface, _: Int ->
+                    if(edittext.text!=null) {
+                        drt.text = edittext.text
+                    }
+                }
+                setNegativeButton("취소"){ _: DialogInterface, _: Int ->
+                    Toast.makeText(context, "적정 탁도 변경 취소", Toast.LENGTH_SHORT).show()
+                }
+                show()
+            }
+        }
+        change_fd.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val builderItem = AlertdialogEdittextBinding.inflate(layoutInflater)
+            val edittext = builderItem.editText
+            with(builder){
+                setTitle("먹이 공급 주기 변경")
+                setMessage("먹이 공급 주기를 입력하세요.")
+                setView(builderItem.root)
+                setPositiveButton("확인"){ _: DialogInterface, _: Int ->
+                    if(edittext.text!=null) {
+                        fd.text = edittext.text
+                    }
+                }
+                setNegativeButton("취소"){ _: DialogInterface, _: Int ->
+                    Toast.makeText(context, "먹이 공급 주기 변경 취소", Toast.LENGTH_SHORT).show()
+                }
+                show()
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
